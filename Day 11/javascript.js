6cca9a2191e6984e23985f22e1df2b44ca9099bb
Joyesh-Santra <joyeshsantra0@gmail.com>
2025-10-28 16:15:45 +0000

    Added the Day 11

---
+++ Day 11/javascript.js
@@ -0,0 +1,36 @@
+let name = "Joyesh"
+let surname = "Santra"
+
+console.log(name)
+console.log(surname)
+
+let age = 54
+
+console.log(age)
+
+age = 11
+
+console.log(age)
+
+// const pi = 3.14
+
+// pi = 11
+
+// console.log(pi)
+
+console.log((3 + 2) - 76 * (1 + 1));
+console.log(23+97)
+
+console.log(23+45+75+23+23+95)
+
+
+let a = 10
+console.log(a)
+
+const max = 57
+
+const actual = max - 13
+
+const percentage  = actual/max
+
+console.log(percentage)
---
+++ Day 11/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const pi = 3.14
+
+// pi = 11
+
+// console.log(pi)
